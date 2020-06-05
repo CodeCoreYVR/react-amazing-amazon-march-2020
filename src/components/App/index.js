de6445fb8104b0ce47955057e39c2ea06a306bdd
Brandon Y Lam <me@brandonlam.ca>
2020-06-05 10:18:05 -0700

    setup React-Router-Dom

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import ProductIndexPage from '../ProductIndexPage';
+
 
 class App extends Component {
   render() {
     return(
       <div>
-        <ProductIndexPage/>
+        <BrowserRouter>
+          <Route path='/products' component={ProductIndexPage}/>
+        </BrowserRouter>
       </div>
     )
   }
