e3295ec3e2324ebe4d3ac2fc27714b2a7e5ce265
Brandon Y Lam <me@brandonlam.ca>
2020-06-05 12:00:27 -0700

    add auth

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,23 +2,48 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ProductIndexPage from '../ProductIndexPage';
 import ProductNewPage from '../ProductNewPage';
+import SignInPage from '../SignInPage';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {}
+    }
+    this.getCurrentUser = this.getCurrentUser.bind(this);
+    this.handleSignIn = this.handleSignIn.bind(this);
+  }
+
   componentDidMount() {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser() {
+    fetch(`http://localhost:3000/api/v1/users/current`, {
+      credentials: 'include'
+    }).then((res) => {
+      return res.json()
+    }).then((data) => {
+      this.setState((state) => {
+        return {
+          currentUser: data
+        }
+      })
+    })
+  }
+
+  handleSignIn(params) {
     fetch(`http://localhost:3000/api/v1/sessions`, {
       method: 'POST',
       credentials: 'include',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        email: 'js@winterfell.gov',
-        password: 'supersecret'
-      })
+      body: JSON.stringify(params)
     }).then((res) => {
       return res.json()
     }).then((data) => {
-      console.log(data);
+      this.getCurrentUser()
     })
   }
   render() {
@@ -28,6 +53,7 @@ class App extends Component {
           <Switch>
             <Route path='/products' exact component={ProductIndexPage}/>
             <Route path='/products/new' component={ProductNewPage}/>
+            <Route path='/sign_in' render={(routeProps) => <SignInPage handleSignIn={this.handleSignIn} {...routeProps}/>} />
           </Switch>
         </BrowserRouter>
       </div>
